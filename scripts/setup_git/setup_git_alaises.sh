7ef1921a369a8ee5bbacff99582d0ddafa9d1916
outslept <135520429+outslept@users.noreply.github.com>
2025-03-10 00:21:28 +0300

    feat: scripts for settings git aliases

---
+++ scripts/setup_git/setup_git_alaises.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git config --global alias.undo 'reset --soft HEAD~1'
+
+git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+
+git config --global alias.s 'status -s'
+
+git config --global alias.last 'log -1 HEAD --stat'
+
+git config --global alias.discard 'checkout -- .'
+
+git config --global alias.ac '!git add -A && git commit -m'
+
+git config --global alias.undo-first 'update-ref -d HEAD'
+
+echo "Git aliases have been successfully configured!"
---
+++ scripts/setup_git/setup_git_aliases.ps1
@@ -0,0 +1,15 @@
+﻿git config --global alias.undo 'reset --soft HEAD~1'
+
+git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+
+git config --global alias.s 'status -s'
+
+git config --global alias.last 'log -1 HEAD --stat'
+
+git config --global alias.discard 'checkout -- .'
+
+git config --global alias.ac '!git add -A && git commit -m'
+
+git config --global alias.undo-first 'update-ref -d HEAD'
+
+Write-Host "Git aliases have been successfully configured!"
