98746f4c038511db40638d76f0dbbdd1c47359ed
outslept <135520429+outslept@users.noreply.github.com>
2025-06-11 13:14:50 +0300

    feat: updating the script to match the fs structure

--- scripts/generate_plugin_list.py
+++ scripts/generate_plugin_list.py
@@ -1,7 +1,11 @@
 import subprocess
 import os
+from pathlib import Path
 
 vscode = 'code.cmd' if os.name == 'nt' else 'code'
+
+config_dir = Path(__file__).parent.parent / 'config' / 'vscode'
+config_dir.mkdir(parents=True, exist_ok=True)
 
 extensions = sorted(subprocess.run(
     [vscode, '--list-extensions'],
@@ -9,5 +13,5 @@ extensions = sorted(subprocess.run(
     text=True
 ).stdout.splitlines())
 
-with open('plugin-list.txt', 'w', encoding='utf-8') as f:
+with open(config_dir / 'plugin-list.txt', 'w', encoding='utf-8') as f:
     f.write('\n'.join(extensions))
